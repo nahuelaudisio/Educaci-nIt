5b78dca5c3345a9ce120419e3efcdb035bc712b7
Nahuel Audisio <nahuel.audisio.95@gmail.com>
2025-03-30 16:19:30 -0300

    primer commit

---
+++ src/proyecto1/main.java
@@ -0,0 +1,14 @@
+package proyecto1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 System.out.println("¡Hola, mundo!");
+		 System.out.println("como están?");
+		 
+
+	}
+
+}
